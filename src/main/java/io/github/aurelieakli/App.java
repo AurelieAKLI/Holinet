fe399173fd7735f2b014066387b5647125089a1a
AurelieAKLI <pr1temps@live.fr>
2022-06-12 18:44:00 +0200

    Add Neo4J lib

--- src/main/java/io/github/aurelieakli/App.java
+++ src/main/java/io/github/aurelieakli/App.java
@@ -1,8 +1,12 @@
 package io.github.aurelieakli;
 
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+
 public class App {
     
     public static void main(String[] args) {
+        Driver driver = GraphDatabase.driver("localhost");
     }
     
 }
